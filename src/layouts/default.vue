f7a46d62f841edf9702425bc63469666807b58c0
Mark Fischer, Jr. <flyingfisch@toppagedesign.com>
2025-05-31 10:41:17 -0400

    Setting up scaffolding

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,11 +1,22 @@
 <template>
-  <v-main>
-    <router-view />
-  </v-main>
+  <v-layout>
+    <v-app-bar title="AI Powered Recipes"></v-app-bar>
+
+    <v-navigation-drawer>
+      <v-list nav>
+        <v-list-item title="Recipes" link></v-list-item>
+        <v-list-item title="Shopping List" link></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-layout>
 
   <AppFooter />
 </template>
 
 <script lang="ts" setup>
-  //
+//
 </script>
