a50df8dc155d21805663791c6f9d9a2eaf94f8ce
Mark Fischer, Jr <flyingfisch@toppagedesign.com>
2025-05-31 11:52:27 -0400

    Added cuisines dropdown and generate recipes button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,30 @@
 <template>
-  <v-container class="fill-height" max-width="900">
+  <v-container>
     <h1>App Content</h1>
+
+    <v-select
+      label="Select Cuisines"
+      v-model="selectedCuisines"
+      items="cuisines"
+      multiple
+    ></v-select>
+
+    <v-btn>Generate Recipes!</v-btn>
   </v-container>
+
 </template>
 
 <script setup lang="ts">
+const selectedCuisines = ref<string[]>([]);
+
+const cuisines = ref<string[]>([
+  'Italian',
+  'Chinese',
+  'Mexican',
+  'Indian',
+  'French',
+  'Japanese',
+  'Mediterranean',
+  'American'
+]);
 </script>
